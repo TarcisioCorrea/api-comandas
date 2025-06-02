0503ece5ace1e8fcd29576973a14da85c76c55a2
Tarcisio Correa <tarcisiosicorrea@gmail.com>
2025-06-02 19:25:21 -0300

    criando as rotas, cliente, produto e funcionario

--- src/main.py
+++ src/main.py
@@ -2,7 +2,17 @@ from fastapi import FastAPI
 from settings import HOST, PORT, RELOAD
 import uvicorn
 
+# import das classes com as rotas/endpoints
+from app import FuncionarioDAO
+from app import ClienteDAO
+from app import ProdutoDAO
+
 app = FastAPI()
+
+# mapeamento das rotas/endpoints
+app.include_router(FuncionarioDAO.router)
+app.include_router(ClienteDAO.router)
+app.include_router(ProdutoDAO.router)
 
 if __name__ == "__main__":
     uvicorn.run('main:app', host=HOST, port=int(PORT), reload=RELOAD)
