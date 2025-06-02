57bd99ff65c02df94cd2df7c060a2a29f9baea7f
Tarcisio Correa <tarcisiosicorrea@gmail.com>
2025-06-02 19:54:28 -0300

    tornando rotas assíncronas

--- src/app/ProdutoDAO.py
+++ src/app/ProdutoDAO.py
@@ -5,23 +5,23 @@ router = APIRouter()
 
 # Criar os endpoints de Cliente: GET, POST, PUT, DELETE
 @router.get("/produto/", tags=["Produto"])
-def get_produto():
+async def get_produto():
     return {"msg": "get todos executado"}, 200
 
 @router.get("/produto/{id}", tags=["Produto"])
-def get_produto(id: int):
+async def get_produto(id: int):
     return {"msg": "get um executado"}, 200
 
 @router.post("/produto/", tags=["Produto"])
-def post_produto(corpo: Produto):
+async def post_produto(corpo: Produto):
     return {"msg": "post executado", "nome": corpo.nome, "descricao": corpo.descricao, "foto":
 corpo.foto, "valor_unitario": corpo.valor_unitario}, 200
 
 @router.put("/produto/{id}", tags=["Produto"])
-def put_produto(id: int, corpo: Produto):
+async def put_produto(id: int, corpo: Produto):
     return {"msg": "put executado", "id":id, "nome": corpo.nome, "descricao": corpo.descricao, "foto":
 corpo.foto, "valor_unitario": corpo.valor_unitario}, 200
 
 @router.delete("/produto/{id}", tags=["Produto"])
-def delete_produto(id: int):
+async def delete_produto(id: int):
     return {"msg": "delete executado"}, 200
